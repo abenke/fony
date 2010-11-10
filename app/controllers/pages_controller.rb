996336514406707e656c2ef711bd0269e6c3784e
alex <alex@alexbenke.com>
2010-11-09 21:17:26 -0500

    static pages first version

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,22 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def exhibit
+    @title = "Exhibit"
   end
 
   def charities
+    @title = "Charities"
+  end
+
+  def about
+    @title = "About"
   end
   
-  def about
+  def donate
+    @title = "Donate"
   end
   
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,32 +1,63 @@
 require 'spec_helper'
 
 describe PagesController do
+  integrate_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
+    it "should have the right title" do
+      get 'home'
+      response.should have_tag("title","Face of NYC | Home")
+    end
   end
 
   describe "GET 'exhibit'" do
     it "should be successful" do
-      get 'contact'
+      get 'exhibit'
       response.should be_success
     end
+    it "should have the right title" do
+      get 'exhibit'
+      response.should have_tag("title","Face of NYC | Exhibit")
+    end
   end
-  
+
   describe "GET 'charities'" do
     it "should be successful" do
-      get 'contact'
+      get 'charities'
       response.should be_success
+    end
+    it "should have the right title" do
+      get 'charities'
+      response.should have_tag("title","Face of NYC | Charities")
+    end
+  end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    it "should have the right title" do
+      get 'about'
+      response.should have_tag("title","Face of NYC | About")
     end
   end
   
-  describe "GET 'about'" do
+  
+  describe "GET 'donate'" do
     it "should be successful" do
-      get 'contact'
+      get 'donate'
       response.should be_success
     end
+    it "should have the right title" do
+      get 'donate'
+      response.should have_tag("title","Face of NYC | Donate")
+    end
   end
+  
+
 end
