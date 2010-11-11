9df717edce6caa42c43df3387f876399206e2fa6
alex <alex@alexbenke.com>
2010-11-10 23:46:44 -0500

    added helpers and railized stylesheet tags to layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def title
+    base_title = "Face of NYC"
+    return "#{base_title} | #{@title}" unless @title.nil?
+    base_title
+  end
 end
